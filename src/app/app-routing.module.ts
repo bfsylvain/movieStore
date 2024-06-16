886f9c18e954c9005e74f0a9862df9b2a5a66c2c
bfsylvain <sylvainbonnaure@hotmail.com>
2024-06-16 23:15:03 +0200

    feat: app routing module set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './modules/home-page/components/features/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/home",
+    pathMatch: "full"
+  },
+  {
+    path: "home",
+    component: HomePageComponent
+  },
+  {
+    path: "movies",
+    loadChildren: ()=> import ("./modules/movie/movie.module")
+    .then(m => m.MovieModule)
+  },
+  {
+    path: "directors",
+    loadChildren: ()=> import("./modules/director/director.module")
+    .then(m => m.DirectorModule)
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
