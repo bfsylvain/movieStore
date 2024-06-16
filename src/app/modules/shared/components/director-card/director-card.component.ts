1e10d234443fa346aec0ff20e54bb08ee9d2f237
bfsylvain <sylvainbonnaure@hotmail.com>
2024-06-16 23:12:19 +0200

    feat: director-card & movie-card created

---
+++ src/app/modules/shared/components/director-card/director-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-director-card',
+  templateUrl: './director-card.component.html',
+  styleUrl: './director-card.component.scss'
+})
+export class DirectorCardComponent {
+
+}
