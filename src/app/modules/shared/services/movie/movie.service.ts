c7097901fcef8218adfe22e758dabd43db701921
bfsylvain <sylvainbonnaure@hotmail.com>
2024-06-16 23:13:02 +0200

    feat: services & environments created

---
+++ src/app/modules/shared/services/movie/movie.service.ts
@@ -0,0 +1,67 @@
+import { Injectable, inject } from '@angular/core';
+import { environment } from '../../../../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, map, switchMap, tap } from 'rxjs';
+import { MovieShortDTO } from '../../models/types/movie-short-DTO.type';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MovieService {
+  private _http = inject(HttpClient);
+  private readonly _BASE_URL = environment.baseUrl + '/movies';
+
+  private allMovieList: MovieShortDTO[] = [];
+  private movies$: BehaviorSubject<MovieShortDTO[]> = new BehaviorSubject(
+    [] as MovieShortDTO[]
+  );
+
+  constructor() {}
+
+  
+  
+  fetchMovies$(): Observable<MovieShortDTO[]> {
+    return this._http.get<MovieShortDTO[]>(this._BASE_URL + '/get/all').pipe(
+      tap((response) => {
+        this.allMovieList = response;
+        this.movies$.next(response);
+      })
+    );
+  }
+  
+  getAllMovies(): void {
+    this.movies$.next(this.allMovieList)
+  }
+  // getAllMovies$(): Observable<MovieShortDTO[]> {
+  //   return this._http.get<MovieShortDTO[]>(this._BASE_URL + '/get/all').pipe(
+  //     tap((response) => {
+  //       this.movies$.next(response);
+  //     })
+  //   );
+  // }
+
+  getWatchedMovies(): void {
+    const watchedMovies = this.allMovieList.filter(movie => movie.isSeen === true)
+    this.movies$.next(watchedMovies)
+  }
+  // getWatchedMovies$(): Observable<MovieShortDTO[]> {
+  //   return this._http
+  //     .get<MovieShortDTO[]>(this._BASE_URL + '/get/watched')
+  //     .pipe(tap((result) => this.movies$.next(result)));
+  // }
+
+  getnotWatchedMovies(): void {
+    const watchedMovies = this.allMovieList.filter(movie => movie.isSeen ===false)
+    this.movies$.next(watchedMovies)
+
+  }
+  // getNotWatchedMovies$(): Observable<MovieShortDTO[]> {
+  //   return this._http
+  //     .get<MovieShortDTO[]>(this._BASE_URL + '/get/not-watched')
+  //     .pipe(tap((result) => this.movies$.next(result)));
+  // }
+
+  getMovieList$(): Observable<MovieShortDTO[]> {
+    return this.movies$.asObservable();
+  }
+}
---
+++ src/app/modules/shared/services/navigation/navigation.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { PageNavigation } from '../../models/types/page-navigation.type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavigationService {
+
+  isSidebarVisible$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
+  PageNavigationList: PageNavigation[] = [
+    { name: 'Accueil', url: 'home' },
+    { name: 'Films', url: 'movies' },
+    { name: 'Réalisateurs', url: 'directors' },
+  ];
+
+  constructor() { }
+
+  setSidebarVisible(): void {
+    this.isSidebarVisible$.next(!this.isSidebarVisible$.value);
+  }
+
+  getSidebarIsVisible$(): Observable<boolean> {
+    return this.isSidebarVisible$.asObservable();
+  }
+
+  setPageNavigationList(): PageNavigation[] {
+    return this.PageNavigationList;
+  }
+
+}
