1b8cbbb82abd0066d1ce251649fc87ece4019ce3
bfsylvain <sylvainbonnaure@hotmail.com>
2024-06-17 16:47:32 +0200

    feat: update go-back & movie display with picture

--- src/app/modules/movie/components/features/movies-to-watch/movies-to-watch.component.ts
+++ src/app/modules/movie/components/features/movies-to-watch/movies-to-watch.component.ts
@@ -3,22 +3,34 @@ import { MovieService } from '../../../../shared/services/movie/movie.service';
 import { Observable } from 'rxjs';
 import { MovieShortDTO } from '../../../../shared/models/types/movie-short-DTO.type';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movies-to-watch',
   templateUrl: './movies-to-watch.component.html',
-  styleUrl: './movies-to-watch.component.scss'
+  styleUrl: './movies-to-watch.component.scss',
 })
 export class MoviesToWatchComponent {
+  private _movieService = inject(MovieService);
+  private _destroyRef: DestroyRef = inject(DestroyRef);
+  private _router = inject(Router)
 
-  private _movieService = inject(MovieService)
-  private _destroyRef: DestroyRef = inject(DestroyRef)
-
-  movieList$: Observable<MovieShortDTO[]> = this._movieService.getNotSeenMovieList$()
+  movieList$: Observable<MovieShortDTO[]> =
+    this._movieService.getNotSeenMovieList$();
 
   ngOnInit(): void {
-    this._movieService.getNotSeenMovies$()
-    .pipe(takeUntilDestroyed(this._destroyRef))
-    .subscribe()
+    this._movieService
+      .getNotSeenMovies$()
+      .pipe(takeUntilDestroyed(this._destroyRef))
+      .subscribe();
   }
+
+  goToMoviePage(movieId: number): void {
+    this._router.navigate([`movies/home/movie/${movieId}`])
+  }
+
+  deleteMovie(id: number) {
+    this._movieService.deleteMovie(id)
+  }
+
 }
--- src/app/modules/movie/components/features/seen-movies/seen-movies.component.ts
+++ src/app/modules/movie/components/features/seen-movies/seen-movies.component.ts
@@ -3,6 +3,7 @@ import { MovieService } from '../../../../shared/services/movie/movie.service';
 import { MovieShortDTO } from '../../../../shared/models/types/movie-short-DTO.type';
 import { Observable } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-seen-movies',
@@ -13,6 +14,7 @@ export class SeenMoviesComponent {
 
   private _movieService = inject(MovieService)
   private _destroyRef: DestroyRef = inject(DestroyRef)
+  private _router = inject(Router)
 
   movieList$: Observable<MovieShortDTO[]> = this._movieService.getWatchedMovieList$()
 
@@ -22,4 +24,11 @@ export class SeenMoviesComponent {
     .subscribe()
   }
 
+  goToMoviePage(movieId: number): void {
+    this._router.navigate([`movies/home/movie/${movieId}`])
+  }
+
+  deleteMovie(id: number) {
+    this._movieService.deleteMovie(id)
+  }
 }
