4ebbe5006a4f78fa8ce1157ed53f487b0319330d
bfsylvain <sylvainbonnaure@hotmail.com>
2024-06-17 09:28:28 +0200

    feat: movie homepage created

---
+++ src/app/modules/movie/components/movie-home-page/movie-home-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-home-page',
+  templateUrl: './movie-home-page.component.html',
+  styleUrl: './movie-home-page.component.scss'
+})
+export class MovieHomePageComponent {
+
+}
