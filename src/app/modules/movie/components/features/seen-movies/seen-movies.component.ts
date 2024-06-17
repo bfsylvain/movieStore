6b620fe49de42d740a21fc1f6d80d3c8446b4846
bfsylvain <sylvainbonnaure@hotmail.com>
2024-06-17 13:44:18 +0200

    feat: seen-movies component created

--- src/app/modules/movie/components/features/seen-movies/seen-movies.component.ts
+++ src/app/modules/movie/components/features/seen-movies/seen-movies.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
+import { MovieService } from '../../../../shared/services/movie/movie.service';
+import { MovieShortDTO } from '../../../../shared/models/types/movie-short-DTO.type';
+import { Observable } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-seen-movies',
@@ -6,5 +10,16 @@ import { Component } from '@angular/core';
   styleUrl: './seen-movies.component.scss'
 })
 export class SeenMoviesComponent {
+
+  private _movieService = inject(MovieService)
+  private _destroyRef: DestroyRef = inject(DestroyRef)
+
+  movieList$: Observable<MovieShortDTO[]> = this._movieService.getWatchedMovieList$()
+
+  ngOnInit(): void {
+    this._movieService.getWatchedMovies$()
+    .pipe(takeUntilDestroyed(this._destroyRef))
+    .subscribe()
+  }
 
 }
