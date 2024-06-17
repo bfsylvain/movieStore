66ebd46ddf005822379cdabe75d24e5dd7c6d9f9
bfsylvain <sylvainbonnaure@hotmail.com>
2024-06-17 09:57:22 +0200

    feat: movie-home-page created

--- src/app/modules/movie/components/movie-home-page/movie-home-page.component.ts
+++ src/app/modules/movie/components/movie-home-page/movie-home-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PageNavigation } from '../../../shared/models/types/page-navigation.type';
+import { NavigationService } from '../../../shared/services/navigation/navigation.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-home-page',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './movie-home-page.component.scss'
 })
 export class MovieHomePageComponent {
+
+  private _navService= inject(NavigationService);
+  private _router= inject(Router)
+
+  pagesToNavigate: PageNavigation[] = this._navService.setMoviePageNavigationList()
+
+
+  navigateTo(url: string): void {
+    this._router.navigateByUrl(url)
+  }
 
 }
