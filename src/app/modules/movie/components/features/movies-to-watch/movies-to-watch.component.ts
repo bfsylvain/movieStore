d0a48719157e7d906a89b5cddd6e10b70669a2c3
bfsylvain <sylvainbonnaure@hotmail.com>
2024-06-17 09:55:51 +0200

    feat: movies to watch component created

---
+++ src/app/modules/movie/components/features/movies-to-watch/movies-to-watch.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movies-to-watch',
+  templateUrl: './movies-to-watch.component.html',
+  styleUrl: './movies-to-watch.component.scss'
+})
+export class MoviesToWatchComponent {
+
+}
