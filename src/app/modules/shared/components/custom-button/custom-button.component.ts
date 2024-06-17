f918468beca90215e41e6c668c672563266a5ab9
bfsylvain <sylvainbonnaure@hotmail.com>
2024-06-17 09:11:13 +0200

    feat: custom button created

---
+++ src/app/modules/shared/components/custom-button/custom-button.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-button',
+  templateUrl: './custom-button.component.html',
+  styleUrl: './custom-button.component.scss'
+})
+export class CustomButtonComponent {
+
+  @Input() name!: string;
+  @Input() style!: string;
+
+}
