957e90bd7b6c8cdbd37a5d74b92d67b63fd4f319
bfsylvain <sylvainbonnaure@hotmail.com>
2024-06-17 13:45:24 +0200

    movie service properties added

--- src/app/modules/shared/services/movie/movie.service.ts
+++ src/app/modules/shared/services/movie/movie.service.ts
@@ -1,26 +1,32 @@
-import { Injectable, inject } from '@angular/core';
+import { DestroyRef, Injectable, inject } from '@angular/core';
 import { environment } from '../../../../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, map, switchMap, tap } from 'rxjs';
 import { MovieShortDTO } from '../../models/types/movie-short-DTO.type';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MovieDTO } from '../../models/types/movie-DTO.type';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MovieService {
   private _http = inject(HttpClient);
+  private _destroyRef: DestroyRef = inject(DestroyRef)
   private readonly _BASE_URL = environment.baseUrl + '/movies';
 
   private allMovieList: MovieShortDTO[] = [];
+  private notSeenMovieList: MovieShortDTO[] = [];
+  private watchedMovieList: MovieShortDTO[] = [];
   private movies$: BehaviorSubject<MovieShortDTO[]> = new BehaviorSubject(
     [] as MovieShortDTO[]
   );
-
+  private notSeenMovies$: BehaviorSubject<MovieShortDTO[]> =
+    new BehaviorSubject([] as MovieShortDTO[]);
+  private watchedMovies$: BehaviorSubject<MovieShortDTO[]> =
+    new BehaviorSubject([] as MovieShortDTO[]);
   constructor() {}
 
-  
-  
-  fetchMovies$(): Observable<MovieShortDTO[]> {
+  getAllMovies$(): Observable<MovieShortDTO[]> {
     return this._http.get<MovieShortDTO[]>(this._BASE_URL + '/get/all').pipe(
       tap((response) => {
         this.allMovieList = response;
@@ -28,40 +34,58 @@ export class MovieService {
       })
     );
   }
-  
-  getAllMovies(): void {
-    this.movies$.next(this.allMovieList)
-  }
-  // getAllMovies$(): Observable<MovieShortDTO[]> {
-  //   return this._http.get<MovieShortDTO[]>(this._BASE_URL + '/get/all').pipe(
-  //     tap((response) => {
-  //       this.movies$.next(response);
-  //     })
-  //   );
-  // }
 
-  getWatchedMovies(): void {
-    const watchedMovies = this.allMovieList.filter(movie => movie.isSeen)
-    this.movies$.next(watchedMovies)
+  getWatchedMovies$(): Observable<MovieShortDTO[]> {
+    return this._http
+      .get<MovieShortDTO[]>(this._BASE_URL + '/get/watched')
+      .pipe(
+        tap((result) => {
+          this.watchedMovieList = result;
+          this.watchedMovies$.next(result);
+        })
+      );
   }
-  // getWatchedMovies$(): Observable<MovieShortDTO[]> {
-  //   return this._http
-  //     .get<MovieShortDTO[]>(this._BASE_URL + '/get/watched')
-  //     .pipe(tap((result) => this.movies$.next(result)));
-  // }
 
-  getnotWatchedMovies(): void {
-    const watchedMovies = this.allMovieList.filter(movie => !movie.isSeen)
-    this.movies$.next(watchedMovies)
-
+  getNotSeenMovies$(): Observable<MovieShortDTO[]> {
+    return this._http
+      .get<MovieShortDTO[]>(this._BASE_URL + '/get/not-watched')
+      .pipe(
+        tap((result) => {
+          this.notSeenMovieList = result;
+          this.notSeenMovies$.next(result);
+        })
+      );
   }
-  // getNotWatchedMovies$(): Observable<MovieShortDTO[]> {
-  //   return this._http
-  //     .get<MovieShortDTO[]>(this._BASE_URL + '/get/not-watched')
-  //     .pipe(tap((result) => this.movies$.next(result)));
-  // }
 
   getMovieList$(): Observable<MovieShortDTO[]> {
     return this.movies$.asObservable();
+  }
+
+  getMovieById(movieId: number): Observable<MovieDTO> {
+    return this._http.get<MovieDTO>(this._BASE_URL + `/get/${movieId}`)
+  }
+
+  getNotSeenMovieList$(): Observable<MovieShortDTO[]> {
+    return this.notSeenMovies$.asObservable();
+  }
+
+  getWatchedMovieList$(): Observable<MovieShortDTO[]> {
+    return this.watchedMovies$.asObservable();
+  }
+
+  deleteMovie(id: number): void {
+    this.allMovieList = this.allMovieList.filter((movie) => movie.id !== id);
+    this.movies$.next(this.allMovieList);
+    this.watchedMovieList = this.watchedMovieList.filter(
+      (movie) => movie.id !== id
+    );
+    this.watchedMovies$.next(this.watchedMovieList);
+    this.notSeenMovieList = this.notSeenMovieList.filter(
+      (movie) => movie.id !== id
+    );
+    this.notSeenMovies$.next(this.notSeenMovieList);
+    this._http.delete(this._BASE_URL + `/delete/${id}`)
+    .pipe(takeUntilDestroyed(this._destroyRef))
+    .subscribe();
   }
 }
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MovieCardComponent } from './components/movie-card/movie-card.component';
 import { DirectorCardComponent } from './components/director-card/director-card.component';
 import { CustomButtonComponent } from './components/custom-button/custom-button.component';
+import { BackToPreviousPageDirective } from './directives/back-to-previous-page.directive';
 
 
 
@@ -10,13 +11,15 @@ import { CustomButtonComponent } from './components/custom-button/custom-button.
   declarations: [
     MovieCardComponent,
     DirectorCardComponent,
-    CustomButtonComponent
+    CustomButtonComponent,
+    BackToPreviousPageDirective
   ],
   imports: [
     CommonModule,
   ],
   exports: [
-    CustomButtonComponent
+    CustomButtonComponent,
+    BackToPreviousPageDirective
   ]
 })
 export class SharedModule { }
