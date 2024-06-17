e76c84a30b9126d8b1ff39453e9767beefd3181b
bfsylvain <sylvainbonnaure@hotmail.com>
2024-06-17 13:43:53 +0200

    feat: movie-page diplay elements

--- src/app/modules/movie/components/features/movie-page/movie-page.component.ts
+++ src/app/modules/movie/components/features/movie-page/movie-page.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MovieService } from '../../../../shared/services/movie/movie.service';
+import { MovieDTO } from '../../../../shared/models/types/movie-DTO.type';
 
 @Component({
   selector: 'app-movie-page',
   templateUrl: './movie-page.component.html',
   styleUrl: './movie-page.component.scss'
 })
-export class MoviePageComponent {
+export class MoviePageComponent implements OnInit {
 
+  private _route = inject(ActivatedRoute)
+  private _movieService = inject(MovieService)
+  private _id!: number;
+  public movie!: MovieDTO;
+  private _router = inject(Router)
+  public isCommentAreaVisible: boolean = false
+
+ngOnInit(): void {
+  this._id= Number(this._route.snapshot.paramMap.get('id'));
+  this._movieService.getMovieById(this._id).subscribe((movie:MovieDTO) => this.movie = movie)
+
+}
+
+goToDirectorPage(directorId: number) {
+  this._router.navigateByUrl(`/directors/director/${directorId}`)
+}
+
+showCommentArea(): void {
+  this.isCommentAreaVisible = !this.isCommentAreaVisible
+}
+sendComment(): void {
+  console.log("comment")
+}
 }
