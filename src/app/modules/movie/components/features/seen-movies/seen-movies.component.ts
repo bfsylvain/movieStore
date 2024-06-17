601f39e5b4aef1611da29ee9aa52ea554b715be2
bfsylvain <sylvainbonnaure@hotmail.com>
2024-06-17 09:56:09 +0200

    feat: seen movies component created

---
+++ src/app/modules/movie/components/features/seen-movies/seen-movies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seen-movies',
+  templateUrl: './seen-movies.component.html',
+  styleUrl: './seen-movies.component.scss'
+})
+export class SeenMoviesComponent {
+
+}
