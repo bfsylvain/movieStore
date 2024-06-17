dbf971785a5b693984ea6b4fd4981aea4f3fbbc7
bfsylvain <sylvainbonnaure@hotmail.com>
2024-06-17 09:56:45 +0200

    feat: movies routing module update

--- src/app/modules/movie/movie-routing.module.ts
+++ src/app/modules/movie/movie-routing.module.ts
@@ -3,25 +3,42 @@ import { RouterModule, Routes } from '@angular/router';
 import { MovieListComponent } from './components/features/movie-list/movie-list.component';
 import { MoviePageComponent } from './components/features/movie-page/movie-page.component';
 import { AddMoviePageComponent } from './components/features/add-movie-page/add-movie-page.component';
+import { MovieHomePageComponent } from './components/movie-home-page/movie-home-page.component';
+import { MoviesToWatchComponent } from './components/features/movies-to-watch/movies-to-watch.component';
+import { SeenMoviesComponent } from './components/features/seen-movies/seen-movies.component';
 
 const routes: Routes = [
   {
     path:"",
-    redirectTo:"all",
+    redirectTo:"home",
     pathMatch: "full"
   },
   {
-    path:"all",
-    component: MovieListComponent
+    path: "home",
+    component: MovieHomePageComponent,
+    children: [
+      {
+        path:"all",
+        component: MovieListComponent
+      },
+      {
+        path: "to-watch",
+        component: MoviesToWatchComponent
+      },
+      {
+        path: "seen",
+        component: SeenMoviesComponent
+      },
+      {
+        path: "movie/:id",
+        component: MoviePageComponent
+      },
+      {
+        path: "add-movie",
+        component: AddMoviePageComponent
+      }    
+    ]
   },
-  {
-    path: "movie/:id",
-    component: MoviePageComponent
-  },
-  {
-    path: "add-movie",
-    component: AddMoviePageComponent
-  }
 ];
 
 @NgModule({
--- src/app/modules/movie/movie.module.ts
+++ src/app/modules/movie/movie.module.ts
@@ -4,6 +4,9 @@ import { MovieRoutingModule } from './movie-routing.module';
 import { MovieListComponent } from './components/features/movie-list/movie-list.component';
 import { MoviePageComponent } from './components/features/movie-page/movie-page.component';
 import { AddMoviePageComponent } from './components/features/add-movie-page/add-movie-page.component';
+import { MovieHomePageComponent } from './components/movie-home-page/movie-home-page.component';
+import { SeenMoviesComponent } from './components/features/seen-movies/seen-movies.component';
+import { MoviesToWatchComponent } from './components/features/movies-to-watch/movies-to-watch.component';
 
 
 @NgModule({
@@ -11,6 +14,9 @@ import { AddMoviePageComponent } from './components/features/add-movie-page/add-
     MovieListComponent,
     MoviePageComponent,
     AddMoviePageComponent,
+    MovieHomePageComponent,
+    SeenMoviesComponent,
+    MoviesToWatchComponent,
   ],
   imports: [
     CommonModule,
